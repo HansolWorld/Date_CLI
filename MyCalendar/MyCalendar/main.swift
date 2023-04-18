5a5b58b1247b2bae24deb6a0f9fa890c526ef3a3
demiiann <tkim0156@gmail.com>
2023-04-18 21:26:50 +0900

    feat: 기능 구현 완료

--- MyCalendar/MyCalendar/main.swift
+++ MyCalendar/MyCalendar/main.swift
@@ -27,16 +27,44 @@ class MyCalendar {
     
     func convertStringToDate(year: String, month: String, day: String) -> Date? {
         var dateComponent = DateComponents()
+        
         dateComponent.year = Int(year)
         dateComponent.month = Int(month)
         dateComponent.day = Int(day)
         
-//        let datecomponent = DateComponents(year: Int(year), month: Int(month), day: Int(day))
         guard let customDate = Calendar.current.date(from: dateComponent) else { return nil }
         return customDate
+    }
+    
+    func convertIntegerToDate(year: Int, month: Int, day: Int) -> Date? {
+        var dateComponent = DateComponents()
+        
+        dateComponent.year = year
+        dateComponent.month = month
+        dateComponent.day = day
+        
+        guard let customDate = Calendar.current.date(from: dateComponent) else { return nil }
+        return customDate
+    }
+    
+    func convertDateToKorean(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        return formatter.string(from: date)
     }
 }
 
 let myC = MyCalendar()
 if let time = myC.printDate() { print(time) }
-if let customDate = myC.convertStringToDate(year: "2023", month: "04", day: "18") { print(customDate) }
+if let customStringDate = myC.convertStringToDate(year: "2023", month: "04", day: "18") {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy MM dd"
+    print(formatter.string(from: customStringDate))
+}
+if let customIntegerDate = myC.convertIntegerToDate(year: 2023, month: 04, day: 19) {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyy MM dd"
+    print(formatter.string(from: customIntegerDate))
+}
+print(myC.convertDateToKorean(date: Date()))
